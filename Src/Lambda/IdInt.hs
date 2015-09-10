98876195179752cce8ceff6a60b22add44932554
Rick Klomp <r.klomp@students.uu.nl>
2015-09-10 20:52:49 +0200

    Lamda evaluation - v 0.1

---
+++ Src/Lambda/IdInt.hs
@@ -0,0 +1,42 @@
+module Src.Lambda.IdInt where
+
+import qualified Data.Map as M
+import Control.Monad.State
+import Src.Lambda.LambdaType
+
+newtype IdInt = IdInt Int
+  deriving (Eq, Ord)
+
+firstBoundId :: IdInt
+firstBoundId = IdInt 0
+
+instance Enum IdInt where
+  toEnum i = IdInt i
+  fromEnum (IdInt i) = i
+
+instance Show IdInt where
+  show (IdInt i) = if i < 0 then "f" ++ show (-i) else "x" ++ show i
+
+toIdInt :: (Ord v) => LC v -> LC IdInt
+toIdInt e = evalState (conv e) (0, fvmap)
+  where fvmap = foldr (\(v, i) m -> M.insert v (IdInt (-i)) m)
+                      M.empty
+                      (zip (freeVars e) [1..])
+
+-- A state monad that has the next unused Int and a mapping of identifiers to IdInt
+type M v a = State (Int, M.Map v IdInt) a
+
+convVar :: (Ord v) => v -> M v IdInt
+convVar v = do
+  (i, m) <- get
+  case M.lookup v m of
+    Nothing -> do
+      let ii = IdInt i
+      put (i+1, M.insert v ii m)
+      return ii
+    Just ii -> return ii
+
+conv :: (Ord v) => LC v -> M v (LC IdInt)
+conv (Var v) = liftM Var (convVar v)
+conv (Lam v e) = liftM2 Lam (convVar v) (conv e)
+conv (App f a) = liftM2 App (conv f) (conv a)
--- Src/Lambda/Lambda.hs
+++ Src/Lambda/Lambda.hs
@@ -1,45 +1,37 @@
-{-# LANGUAGE GADTs, KindSignatures, RankNTypes, FlexibleContexts, TypeOperators, NoMonomorphismRestriction, TypeFamilies #-}
 module Src.Lambda.Lambda where
 
-data HFix f a = HIn { hout :: f (HFix f) a }
-type ExpT' = HFix ExpTF
-data ExpTF :: (* -> *) -> * -> * where
-  TI      :: Int    -> ExpTF a Int
-  TB      :: Bool   -> ExpTF a Bool
-  TGT     :: a Int  -> a Int        -> ExpTF a Bool
-  TEQ     :: a Int  -> a Int        -> ExpTF a Bool
-  TSucc   :: a Int  -> ExpTF a Int
-  TAdd    :: a Int  -> a Int        -> ExpTF a Int
-  TIf     :: a Bool -> a b          -> a b          -> ExpTF a b
+import Data.List
+import Src.Lambda.LambdaType
+import Src.Lambda.IdInt
 
-class HFunctor f where
-  hfmap :: (forall b. g b -> h b) -> f g a -> f h a
+nf :: LC IdInt -> LC IdInt
+nf e@(Var _) = e
+nf (Lam x e) = Lam x (nf e)
+nf (App f a) =
+  case whnf f of
+    Lam x b -> nf (subst x a b)
+    f' -> App (nf f') (nf a)
 
-hfold :: HFunctor f => (forall b. f r b -> r b) -> HFix f a -> r a
-hfold f = f.hfmap (hfold f) . hout
+whnf e@(Var _) = e
+whnf e@(Lam _ _) = e
+whnf (App f a) =
+  case whnf f of
+    Lam x b -> whnf (subst x a b)
+    f' -> App f' a
 
-newtype Id a = Id { unId :: a }
+subst :: IdInt -> LC IdInt -> LC IdInt -> LC IdInt
+subst x s b = sub b
+  where sub e@(Var v) | v == x = s
+                      | otherwise = e
+        sub e@(Lam v e') | v == x = e
+                         | v `elem` fvs = Lam v' (sub e'')
+                         | otherwise = Lam v (sub e')
+                            where v' = newId vs
+                                  e'' = subst v (Var v') e'
+        sub (App f a) = App (sub f) (sub a)
 
-evalT' :: ExpT' a -> a
-evalT' = unId . hfold evalAlgT
+        fvs = freeVars s
+        vs = fvs `union` allVars b
 
-evalAlgT :: ExpTF Id a -> Id a
-evalAlgT (TI i) = Id $ i
-evalAlgT (TB b) = Id $ b
-evalAlgT (TGT e1 e2) | unId e1 > unId e2 = Id $ True
-                     | otherwise         = Id $ False
-evalAlgT (TIsZero e) | unId e == 0 = Id $ True
-                     | otherwise   = Id $ False
-evalAlgT (TSucc e) = Id $ unId e + 1
-evalAlgT (TAdd e1 e2) = Id $ unId e1 + unId e2
-evalAlgT (TIf e1 e2 e3) | unId e1   = e2
-                        | otherwise = e3
-
-instance HFunctor ExpTF where
-  hfmap eval (TI i) = TI i
-  hfmap eval (TB b) = TB b
-  hfmap eval (TGT e1 e2) = TGT (eval e1) (eval e2)
-  hfmap eval (TIsZero e) = TIsZero (eval e)
-  hfmap eval (TSucc e) = TSucc (eval e)
-  hfmap eval (TAdd e1 e2) = TAdd (eval e1) (eval e2)
-  hfmap eval (TIf e1 e2 e3) = TIf (eval e1) (eval e2) (eval e3)
+newId :: [IdInt] -> IdInt
+newId vs = head ([firstBoundId ..] \\ vs)
